a4fd8b034d747a495bfbe42a67d773f2157b0896
Lerdor <marshal604@gmail.com>
2019-04-28 23:24:30 +0800

    add credit-trade crawler.

--- server/src/services/utils/io/io.ts
+++ server/src/services/utils/io/io.ts
@@ -1,51 +1,40 @@
-import fs from "fs";
+import fs from 'fs';
 
-import { FileModel, IsFileExistModel, writeFileModel } from "./io.model";
+import { FileModel, IsFileExistModel, writeFileModel } from './io.model';
 
-const rootPath = "src/assets";
+const rootPath = 'src/assets';
 export const IsFileExist = ({
   path,
   fileName,
-  fileExtension = "json",
+  fileExtension = 'json',
   log = true
 }: IsFileExistModel): Promise<never> => {
   return new Promise((resolve, reject) => {
-    fs.readFile(
-      `${rootPath}/${path}/${fileName}.${fileExtension}`,
-      "utf-8",
-      (err, data) => {
-        if (err) {
-          if (log) {
-            console.log(
-              `not have ${path}/${fileName}.${fileExtension} file, will download it.`
-            );
-          }
-          reject(err);
-          return;
+    fs.readFile(`${rootPath}/${path}/${fileName}.${fileExtension}`, 'utf-8', (err, data) => {
+      if (err) {
+        if (log) {
+          console.log(`not have ${path}/${fileName}.${fileExtension} file, will download it.`);
         }
-        resolve();
+        reject(err);
+        return;
       }
-    );
+      resolve();
+    });
   });
 };
 
 export const IsFileExistSync = ({
   path,
   fileName,
-  fileExtension = "json",
+  fileExtension = 'json',
   log = true
 }: IsFileExistModel): boolean => {
   try {
-    fs.readFileSync(
-      `${rootPath}/${path}/${fileName}.${fileExtension}`,
-      "utf-8"
-    );
+    fs.readFileSync(`${rootPath}/${path}/${fileName}.${fileExtension}`, 'utf-8');
     return true;
   } catch {
     if (log) {
-      console.log(
-        `not have ${path}/${fileName}.${fileExtension} file, will download it.`
-      );
+      console.log(`not have ${path}/${fileName}.${fileExtension} file, will download it.`);
     }
     return false;
   }
@@ -55,57 +44,37 @@ export const writeFile = ({
   path,
   fileName,
   data,
-  fileExtension = "json"
+  fileExtension = 'json'
 }: writeFileModel): void => {
-  fs.writeFile(
-    `${rootPath}/${path}/${fileName}.${fileExtension}`,
-    data,
-    "utf-8",
-    err => {
-      if (err) {
-        console.log(`write ${path}/${fileName}.${fileExtension} has err`, err);
-        return;
-      }
-      console.log(`write ${path}/${fileName}.${fileExtension}`);
+  fs.writeFile(`${rootPath}/${path}/${fileName}.${fileExtension}`, data, 'utf-8', err => {
+    if (err) {
+      console.log(`write ${path}/${fileName}.${fileExtension} has err`, err);
+      return;
     }
-  );
-};
-
-export const readFile = ({
-  path,
-  fileName,
-  fileExtension = "json"
-}: FileModel): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(
-      `${rootPath}/${path}/${fileName}.${fileExtension}`,
-      "utf-8",
-      (err, data) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(data);
-      }
-    );
+    console.log(`write ${path}/${fileName}.${fileExtension}`);
   });
 };
 
-export const readFileSync = ({
-  path,
-  fileName,
-  fileExtension = "json"
-}: FileModel): {} => {
-  return fs.readFileSync(
-    `${rootPath}/${path}/${fileName}.${fileExtension}`,
-    "utf-8"
-  );
+export const readFile = ({ path, fileName, fileExtension = 'json' }: FileModel): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${rootPath}/${path}/${fileName}.${fileExtension}`, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+};
+
+export const readFileSync = ({ path, fileName, fileExtension = 'json' }: FileModel): {} => {
+  return fs.readFileSync(`${rootPath}/${path}/${fileName}.${fileExtension}`, 'utf-8');
 };
 
 export const removeFile = ({
   path,
   fileName,
-  fileExtension = "json"
+  fileExtension = 'json'
 }: FileModel): Promise<never> => {
   return new Promise((resolve, reject) => {
     fs.unlink(`${rootPath}/${path}/${fileName}.${fileExtension}`, err => {
@@ -120,11 +89,19 @@ export const removeFile = ({
   });
 };
 
-export const removeFileSync = ({
-  path,
-  fileName,
-  fileExtension = "json"
-}: FileModel): void => {
+export const removeFileSync = ({ path, fileName, fileExtension = 'json' }: FileModel): void => {
   console.log(`remove ${path}/${fileName}`);
   return fs.unlinkSync(`${rootPath}/${path}/${fileName}.${fileExtension}`);
+};
+
+export const mkdirIfNotExist = (dir: string) => {
+  const path = `${rootPath}/${dir}`;
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+};
+
+// 111,000 => 111000
+export const formatNumberSymbol = (data: string): string => {
+  return data.split(',').join('');
 };
